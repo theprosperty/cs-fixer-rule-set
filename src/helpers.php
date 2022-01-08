b8e81f95ba2e85a98652f4c92596d1f2e773963a
Np0 <np@devshell.com>
2022-01-08 20:44:26 +0200

    wip

---
+++ src/helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TheProsperty;
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+function styles(Finder $finder, array $rules = []): Config
+{
+    $rules = array_merge(require __DIR__.'/rules.php', $rules);
+
+    return (new Config())->setRules($rules)->setFinder($finder);
+}
